4130596e7cd09e16404b33bd43cb62384cf42043
donlongee@gmail.com <Prestigio1>
2014-11-27 10:23:21 +0100

    gg

--- src/com/company/Artist.java
+++ src/com/company/Artist.java
@@ -4,4 +4,56 @@ package com.company;
  * Created by student14 on 2014-11-27.
  */
 public class Artist {
+    private String name;
+    private String genre;
+    private String country;
+
+                public Artist(String name, String genre, String country) {
+                this.name = name;
+                this.genre = genre;
+                this.country = country;
+                }
+
+
+                public String getArtist() {
+                return artist;
+                }
+
+                public void setArtist(String artist) {
+                this.artist = artist;
+                }
+                public String getName() {
+                return name;
+                }
+
+                public void setName(String name) {
+                this.name = name;
+                }
+
+                public String getGenre() {
+                return genre;
+                }
+
+                public void setGenre(String genre) {
+                this.genre = genre;
+                }
+
+                public String getCountry() {
+                return country;
+                }
+
+                public void setCountry(String country) {
+                this.country = country;
+                }
+
+    public static void main(String[] args) {
+        Artist asap = new PlayList();
+        p.loadSongs("sample.txt");
+        System.out.println(p.getSongList());
+        p.sortByArtist();
+        System.out.println(p.totalPlayTime());
+        p.clear();
+        System.out.println(p.getSongList());
+    }
 }
+
--- src/com/company/Song.java
+++ src/com/company/Song.java
@@ -8,4 +8,61 @@ public class Song {
     private String title;
     private int minutes;
     private int seconds;
+
+                public String getArtist() {
+        		return artist;
+        	    }
+
+            	public void setArtist(String artist) {
+        	    this.artist = artist;
+        	    }
+
+            	public String getTitle() {
+       		    return title;
+        	    }
+
+            	public void setTitle(String title) {
+        		this.title = title;
+        	    }
+
+            	public int getMinutes() {
+        		return minutes;
+        	    }
+
+            	public void setMinutes(int minutes) {
+        		this.minutes = minutes;
+        	    }
+
+            	public int getSeconds() {
+        		return seconds;
+        	    }
+
+            	public void setSeconds(int seconds) {
+        		this.seconds = seconds;
+        	    }
+
+            	public Song(String title, String artist) {
+        		this.title = title;
+        		this.artist = artist;
+        	    }
+
+            	public Song(String title, String artist, int minutes, int seconds) {
+       		    this.title = title;
+        		this.artist = artist;
+        		this.minutes = minutes;
+        		this.seconds = seconds;
+        	    }
+
+            	public Song(Song s){
+       		    this.title = s.title;
+        		this.artist = s.artist;
+        		this.minutes = s.minutes;
+        		this.seconds = s.seconds;
+
+                }
+
+                public String toString() {
+        		return ("{Song: title=" + title + " artist: " + artist + "}");
+                }
+
 }
